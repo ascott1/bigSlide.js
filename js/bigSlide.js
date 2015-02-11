020c8baa63a37e9a26217f95c61b7c8b8caea409
Adam Scott <adam.scott@cfpb.gov>
2015-02-11 06:42:15 -0500

    bump to version 0.6.0

--- js/bigSlide.js
+++ js/bigSlide.js
@@ -1,75 +1,109 @@
-/*! bigSlide - v0.4.3 - 2014-01-25
+/*! bigSlide - v0.6.0 - 2015-02-11
 * http://ascott1.github.io/bigSlide.js/
-* Copyright (c) 2014 Adam D. Scott; Licensed MIT */
+* Copyright (c) 2015 Adam D. Scott; Licensed MIT */
 (function($) {
   'use strict';
 
   $.fn.bigSlide = function(options) {
 
+    // plugin settings
     var settings = $.extend({
       'menu': ('#menu'),
       'push': ('.push'),
       'side': 'left',
       'menuWidth': '15.625em',
-      'speed': '300'
+      'speed': '300',
+      'state': 'closed'
     }, options);
 
-    var menuLink = this,
-        menu = $(settings.menu),
-        push = $(settings.push),
-        width = settings.menuWidth;
-
-    var positionOffScreen = {
-      'position': 'fixed',
-      'top': '0',
-      'bottom': '0',
-      'settings.side': '-' + settings.menuWidth,
-      'width': settings.menuWidth,
-      'height': '100%'
+    // store the menu's state in the model
+    var model = {
+      'state': settings.state
     };
 
-    var animateSlide = {
-      '-webkit-transition': settings.side + ' ' + settings.speed + 'ms ease',
-      '-moz-transition': settings.side + ' ' + settings.speed + 'ms ease',
-      '-ms-transition': settings.side + ' ' + settings.speed + 'ms ease',
-      '-o-transition': settings.side + ' ' + settings.speed + 'ms ease',
-      'transition': settings.side + ' ' + settings.speed + 'ms ease'
-    };
-
-    menu.css(positionOffScreen);
-    push.css(settings.side, '0');
-    menu.css(animateSlide);
-    push.css(animateSlide);
-
-    menu._state = 'closed';
-
-    menu.open = function() {
-      menu._state = 'open';
-      menu.css(settings.side, '0');
-      push.css(settings.side, width);
-    };
-
-    menu.close = function() {
-      menu._state = 'closed';
-      menu.css(settings.side, '-' + width);
-      push.css(settings.side, '0');
-    };
-
-    menuLink.on('click.bigSlide', function(e) {
-      e.preventDefault();
-      if (menu._state === 'closed') {
-        menu.open();
-      } else {
-        menu.close();
+    // talk back and forth between the view and state
+    var controller = {
+      init: function(){
+        view.init();
+      },
+      // update the menu's state
+      changeState: function(){
+        //model.state === 'closed' ? model.state = 'open' : model.state = 'closed';
+        if (model.state === 'closed') {
+          model.state = 'open'
+        } else {
+          model.state = 'closed'
+        }
+      },
+      // check the menu's state
+      getState: function(){
+        return model.state;
       }
-    });
+    };
 
-    menuLink.on('touchend', function(e){
-      menuLink.trigger('click.bigSlide');
-      e.preventDefault();
-    });
+    // the view contains all of the visual interactions
+    var view = {
+      init: function(){
+        // cache DOM values
+        this.$menu = $(settings.menu);
+        this.$push = $(settings.push);
+        this.width = settings.menuWidth;
 
-    return menu;
+        // CSS for how the menu will be positioned off screen
+        var positionOffScreen = {
+          'position': 'fixed',
+          'top': '0',
+          'bottom': '0',
+          'settings.side': '-' + settings.menuWidth,
+          'width': settings.menuWidth,
+          'height': '100%'
+        };
+
+        // css for the sliding animation
+        var animateSlide = {
+          '-webkit-transition': settings.side + ' ' + settings.speed + 'ms ease',
+          '-moz-transition': settings.side + ' ' + settings.speed + 'ms ease',
+          '-ms-transition': settings.side + ' ' + settings.speed + 'ms ease',
+          '-o-transition': settings.side + ' ' + settings.speed + 'ms ease',
+          'transition': settings.side + ' ' + settings.speed + 'ms ease'
+        };
+
+        // add the css values to the menu and 'push' class
+        this.$menu.css(positionOffScreen);
+        this.$push.css(settings.side, '0');
+        this.$menu.css(animateSlide);
+        this.$push.css(animateSlide);
+
+        // register a click listener for desktop & touchstart for mobile
+        $(document).on('click.bigSlide touchstart.bigSlide', function(e) {
+          e.preventDefault();
+          if (controller.getState() === 'open') {
+            view.toggleClose();
+          } else {
+            view.toggleOpen();
+          }
+        });
+      },
+
+      // toggle the menu open
+      toggleOpen: function() {
+        controller.changeState();
+        this.$menu.css(settings.side, '0');
+        this.$push.css(settings.side, this.width);
+        //menuLink.addClass(settings.activeBtn);
+      },
+
+      // toggle the menu closed
+      toggleClose: function() {
+        controller.changeState();
+        this.$menu.css(settings.side, '-' + this.width);
+        this.$push.css(settings.side, '0');
+        //menuLink.removeClass(settings.activeBtn);
+      }
+
+    }
+
+    controller.init();
 
   };
 
