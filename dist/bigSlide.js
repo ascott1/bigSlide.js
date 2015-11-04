278084ce6038a76e7dc6039614eabdfe108ef2fd
Adam Scott <adam.scott@cfpb.gov>
2015-11-04 15:05:35 -0500

    bump to 0.9.3 due to fix dist file publication

--- dist/bigSlide.js
+++ dist/bigSlide.js
@@ -1,4 +1,4 @@
-/*! bigSlide - v0.9.2 - 2015-11-04
+/*! bigSlide - v0.9.3 - 2015-11-04
 * http://ascott1.github.io/bigSlide.js/
 * Copyright (c) 2015 Adam D. Scott; Licensed MIT */
 (function (factory) {
